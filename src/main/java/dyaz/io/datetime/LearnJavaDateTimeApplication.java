5753e80e7ed702fe0082e70979842ae412a856a9
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-07-13 18:57:30 +0700

    Initial Commitgit add .

---
+++ src/main/java/dyaz/io/datetime/LearnJavaDateTimeApplication.java
@@ -0,0 +1,13 @@
+package dyaz.io.datetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaDateTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaDateTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dyaz/io/datetime/LearnJavaDateTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package dyaz.io.datetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaDateTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
